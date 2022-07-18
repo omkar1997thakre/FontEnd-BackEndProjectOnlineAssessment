7583b95af8da1dfcdc120c56453aaf0be24f78c5
omkar1997thakre <thakreom1997@gmail.com>
2022-07-18 12:10:53 +0530

    final code

---
+++ Angular-Quiz-Application-master/src/app/app.component.ts
@@ -0,0 +1,113 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ModalDirective } from '../../node_modules/ngx-bootstrap';
+import { QuestionClass } from './question-class';
+import { ToastrService } from 'ngx-toastr';
+
+
+
+
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+	isQuestionCardShow: boolean = false;
+	totalAnswered: number = 0;
+	rightAnswer: number;
+	questionObj = new QuestionClass();
+	@ViewChild('submitModal') submitModal: ModalDirective;
+	@ViewChild('addQuestionModal') addQuestionModal : ModalDirective;
+	@ViewChild('answerModal') answerModal : ModalDirective;
+	@ViewChild('questionForm') questionForm: any;
+	@ViewChild('questionTest') questionTest : any;
+
+	constructor( private toastr: ToastrService) { }
+
+	answerArray = [];
+
+	allQuestions: any = [{
+		"id": 1,
+		"question": "AngularJS is a_________?",
+		"a": "Java Framework",
+		"b": "Java ScriptFrameweok",
+		"c": "HTML Framework",
+		"d": "SQL framework",
+		"answer": "b"
+	},
+	{
+		"id": 2,
+		"question": "Choose the correct syntax for writing AngularJS expression.?",
+		"a": "[expression]",
+		"b": "{expression}",
+		"c": "{{expression}}",
+		"d": "{{{expression}}}",
+		"answer": "c"
+	},
+	{
+		"id": 3,
+		"question": "Choose the incorrect AngularJS filter.?",
+		"a": "Orderby",
+		"b": "email",
+		"c": "currency",
+		"d": "lowercase",
+		"answer": "b"
+	}
+	];
+
+	/**Method call on submit the test */
+	submitTest() {
+		this.rightAnswer = 0;
+		this.totalAnswered = 0;
+		for (let i = 0; i < this.allQuestions.length; i++) {
+			if ("selected" in this.allQuestions[i] && (this.allQuestions[i]["selected"] != null)) {
+				this.totalAnswered++;
+				if (this.allQuestions[i]["selected"] == this.allQuestions[i]["answer"]) {
+					this.rightAnswer++;
+				}
+			}
+
+		}
+		this.submitModal.show();
+
+	}
+
+	startQuiz() {
+		for (let i = 0; i < this.allQuestions.length; i++) {
+			if ("selected" in this.allQuestions[i]) {
+				delete this.allQuestions[i]["selected"];
+			}
+
+		}
+		this.questionTest.reset();
+		this.isQuestionCardShow = true;
+
+	}
+	HomePage() {
+		this.isQuestionCardShow = false;
+	}
+	addQuestion(){
+		this.addQuestionModal.show();
+	}
+	submitAddQuestion(){
+		let quesTemp = JSON.parse(JSON.stringify(this.questionObj));
+		quesTemp["id"] = this.allQuestions.length+1;
+		this.allQuestions.push(quesTemp);
+		this.questionForm.reset();
+		this.toastr.success("Question Added Successfully!!");
+		this.addQuestionModal.hide();
+
+	}
+	checkAnswers(){
+		this.submitModal.hide();
+		this.answerModal.show();
+	}
+
+	ngOnInit() {
+
+
+
+	}
+
+}
